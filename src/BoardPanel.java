4d4d18a658533ca19f0d2eea935c3ae06e0f2650
Colepark2002 <colepark2002@gmail.com>
2022-11-06 11:35:12 -0800

    Added Super Constructor to Board Panel and fix button colors

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,4 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BoardPanel extends JPanel {
+public class BoardPanel extends JPanel implements ActionListener {
+    JButton[][] board = new JButton[10][10];
+    String[] Alph = {"A","B","C","D","E","F","G","H","I","J"};
+    public BoardPanel()
+    {
+        GridLayout g = new GridLayout(10,10);
+        setLayout(g);
+        for(int i = 0; i < 10; i++)
+        {
+            for(int k = 0; k < 10; k++)
+            {
+                board[i][k] = new JButton(Alph[i] + (1+k));
+                board[i][k].setBackground(Color.LIGHT_GRAY);
+                add(board[i][k]);
+                board[i][k].addActionListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
 }
--- src/PlayerPanel.java
+++ src/PlayerPanel.java
@@ -11,22 +11,9 @@ public class PlayerPanel extends BoardPanel implements MouseListener, ActionList
 
 
     int ShipPieces = 17;
-    JButton[][] board = new JButton[10][10];
-    String[] Alph = {"A","B","C","D","E","F","G","H","I","J"};
 
     public PlayerPanel()
     {
-        GridLayout g = new GridLayout(10,10);
-        setLayout(g);
-        for(int i = 0; i < 10; i++)
-        {
-            for(int k = 0; k < 10; k++)
-            {
-                board[i][k] = new JButton(Alph[i] + (1+k));
-                add(board[i][k]);
-                board[i][k].addActionListener(this);
-            }
-        }
     }
 
     public int getShipPieces() {
