1507d572e9a5a55ce438841242743b0f938d35f5
Jacob Shapero <jwshapero@gmail.com>
2022-11-06 20:39:07 -0800

    Add files via upload

--- src/BlackBoard.java
+++ src/BlackBoard.java
@@ -1,2 +1,35 @@
-public class BlackBoard {
+import javax.swing.*;
+import java.util.Observable;
+
+public class BlackBoard extends Observable {
+    BoardPanel player1Board, player2Board;
+    private static BlackBoard instance;
+
+    protected BlackBoard(){
+        player1Board = new BoardPanel();
+        player2Board = new BoardPanel();
+
+    }
+
+
+    public BoardPanel getBoard(int playerNum){
+        if(playerNum == 1){
+            return player1Board;
+        }
+        else{
+            return player2Board;
+        }
+    }
+
+    public void notifyObservers(){
+
+    }
+
+
+    public static BlackBoard getInstance(){
+        if(instance == null){
+            instance = new BlackBoard();
+        }
+        return instance;
+    }
 }
