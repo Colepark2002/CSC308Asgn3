827a8207a4bf4b5ac7ad53e08507ab29d5b09846
Colepark2002 <colepark2002@gmail.com>
2022-11-06 20:59:25 -0800

    Added Ship Placements to PlayerPanel

--- src/PlayerPanel.java
+++ src/PlayerPanel.java
@@ -1,18 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-public class PlayerPanel extends BoardPanel implements ActionListener {
+public class PlayerPanel extends BoardPanel {
 
 
 
     int ShipPieces = 17;
+    boolean placingShips = true;
+    boolean first = true;
+    int[] start = new int[2];
+    int[] shipSize = {5, 4, 3, 3, 2};
+    int shipInd = 0;
 
     public PlayerPanel()
     {
+        placeShips();
     }
 
     public int getShipPieces() {
@@ -33,25 +36,143 @@ public class PlayerPanel extends BoardPanel implements ActionListener {
 
     }
 
-    public void placeShip()
+    public void placeShips() {
+        JOptionPane.showMessageDialog(this, "Now you can place your ships");
+        JOptionPane.showMessageDialog(this, "Place your ship by clicking a start location and then a direction to place the rest of it");
+        JOptionPane.showMessageDialog(this, "Start by placing your battleship " + shipSize[shipInd] + " X 1");
+    }
+
+    public void placeShip(int[] startIndex, String dir)
     {
+        switch(dir)
+        {
+            case "up":
+                if(startIndex[0] - (shipSize[shipInd] - 1) < 0)
+                {
+                    JOptionPane.showMessageDialog(this, "invalid direction try again");
+                    first = false;
+                }
+                else
+                {
+                    for(int i = 0; i < shipSize[shipInd]; i++)
+                    {
+                        board[startIndex[0]-i][startIndex[1]].setBackground(Color.BLACK);
+                    }
+                }
+                break;
+
+            case "down":
+                if(startIndex[0] + (shipSize[shipInd] - 1) > 9)
+                {
+                    JOptionPane.showMessageDialog(this, "invalid direction try again");
+                    first = false;
+                }
+                else
+                {
+                    for(int i = 0; i < shipSize[shipInd]; i++)
+                    {
+                        board[startIndex[0]+i][startIndex[1]].setBackground(Color.BLACK);
+                    }
+                }
+                break;
+
+            case "left":
+                if(startIndex[1] - (shipSize[shipInd] - 1) < 0)
+                {
+                    JOptionPane.showMessageDialog(this, "invalid direction try again");
+                    first = false;
+                }
+                else
+                {
+                    for(int i = 0; i < shipSize[shipInd]; i++)
+                    {
+                        board[startIndex[0]][startIndex[1]-i].setBackground(Color.BLACK);
+                    }
+                }
+                break;
+
+            case "right":
+                if(startIndex[1] + (shipSize[shipInd] - 1) > 9)
+                {
+                    JOptionPane.showMessageDialog(this, "invalid direction try again");
+                    first = false;
+                }
+                else
+                {
+                    for(int i = 0; i < shipSize[shipInd]; i++)
+                    {
+                        board[startIndex[0]][startIndex[1]+i].setBackground(Color.BLACK);
+                    }
+                }
+                break;
+        }
+        if(first)
+        {
+            shipInd++;
+            if (shipInd >= 5)
+            {
+                placingShips = false;
+                JOptionPane.showMessageDialog(this, "All ships are placed");
+            }
+            else
+                JOptionPane.showMessageDialog(this, "Next place your ship of size " + shipSize[shipInd] + " X 1");
+        }
+
 
     }
+
 
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        int i = 0;
-        int k;
-        while( !e.getActionCommand().substring(0,1).equals(Alph[i]))
+        if(placingShips && !(e.getActionCommand().equals("OK")))
         {
-            i +=1;
+            int i = 0;
+            int k;
+            while( !e.getActionCommand().substring(0,1).equals(Alph[i]))
+            {
+                i +=1;
+            }
+
+            k = Integer.parseInt(e.getActionCommand().substring(1)) - 1;
+            if(first)
+            {
+                start[0] = i;
+                start[1] = k;
+                first = false;
+            }
+            else
+            {
+                first = true;
+                String dir = "";
+
+                if(i > start[0] && k == start[1]) {
+                    dir = "down";
+                    placeShip(start,dir);
+                }
+                else if(i < start[0] && k == start[1]) {
+                    dir = "up";
+                    placeShip(start,dir);
+                }
+                else if(i == start[0] && k < start[1]) {
+                    dir = "left";
+                    placeShip(start,dir);
+                }
+                else if(i == start[0] && k > start[1]) {
+                    dir = "right";
+                    placeShip(start,dir);
+                }
+                else {
+                    JOptionPane.showMessageDialog(this, "invalid direction try again");
+                    first = false;
+                }
+                System.out.println(dir);
+            }
+            System.out.println("Index" + " " + i + " " + k);
+
         }
 
-        k = Integer.parseInt(e.getActionCommand().substring(1)) - 1;
-        board[i][k].setBackground(Color.BLACK);
-        System.out.println("Index" + " " + i + " " + k);
     }
 
 
